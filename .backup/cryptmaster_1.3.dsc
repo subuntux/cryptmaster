abb471360e0988403ef09ade8c12204a7a51d288
subuntux <subuntu314@gmail.com>
2024-03-17 22:53:25 +0100

    create

---
+++ .backup/cryptmaster_1.3.dsc
@@ -0,0 +1,33 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
+
+Format: 1.0
+Source: cryptmaster
+Binary: cryptmaster
+Architecture: all
+Version: 1.3
+Maintainer: Alexander Krefting <kakrefting@gmail.com>
+Build-Depends: debhelper-compat (= 12)
+Package-List:
+ cryptmaster deb utils important arch=all
+Checksums-Sha1:
+ bd608d47fbc1a7559acde1423ae36298d08f9839 1108 cryptmaster_1.3.tar.gz
+Checksums-Sha256:
+ 80c68a9f2452213f541c7c62744720a0088ea967720dd70f67a2ec067d4df1e5 1108 cryptmaster_1.3.tar.gz
+Files:
+ 621c2c3a81cbd691f1e0604673c3b830 1108 cryptmaster_1.3.tar.gz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQGzBAEBCgAdFiEEZqbPTSgZGEpMakPZVAg0d6KegTQFAmX3ZHkACgkQVAg0d6Ke
+gTTUMAwAnW5UoKc7RXOj5fbN/ww2xUtcEnn3yHhqBlCQmIR52ma640rylYRo2KPz
+wPgrHO+3oFROP6Wq1rUvy5Js76iAxxA13ySf3MtmS2mNM4Q986rIo64FAgq+8IFy
+p+Wjr19pNF2uG9dy/aW7rbiO2hZqQO869Ap/W4g5IjmYseh/q+G0Qw2a1IIRJWoA
+4YIDLpnwz51IpCdy7g+pYvFvhDG0nkZO1AnTbNFHvzps/x10ST8CaCV3J+KGHgI8
+r+lQdsFDfs8yYDUGqGNBaxtwsVGIcD5N7scn5qV/Z0D437gXB69SgbAFyfz/DlIF
++910GgeeK9fk9216UYIbs5sPZ6wEefNP6BOh3DiTjfn0+0HT9bcqB1ma+MKG+BG5
+Uzc/VC85jYhtdl816cFtWTENFVd2G/tV9iPjYArL4jK+USAuu8FOAn+LMLDz/rOu
+BEryWyo1DRgpdKXIYizDW0N8EnIIkBVF4QrTP9nt3oXqFh++6g1CoofPa0TVkzrw
+uD45/gjd
+=sJfU
+-----END PGP SIGNATURE-----
---
+++ crypter.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+
+def encrypt_decrypt(input_text, key, mode):
+    output_text = ""
+    for i, char in enumerate(input_text):
+        shift = int(key[i % len(key)])
+        if mode == "--crypt":
+            shifted_char = chr(ord(char) + shift)
+        elif mode == "--decrypt":
+            shifted_char = chr(ord(char) - shift)
+        else:
+            print("Unknown mode.")
+            return None
+        output_text += shifted_char
+    return output_text
+
+def main():
+    if len(sys.argv) < 4 or sys.argv[1] == "--help":
+        print("Usage:")
+        print("crypter --crypt <inputfile> <outputfile>")
+        print("crypter --decrypt <inputfile> <outputfile>")
+        print("crypter --help")
+        return
+    
+    mode = sys.argv[1]
+    input_file = sys.argv[2]
+    output_file = sys.argv[3]
+
+    with open(input_file, 'r') as f:
+        input_text = f.read().replace('\n', '')
+
+    key = input("Please enter a ten-digit number: ")
+    if len(key) != 10 or not key.isdigit():
+        print("Invalid input for the key.")
+        return
+
+    encrypted_decrypted_text = encrypt_decrypt(input_text, key, mode)
+
+    if encrypted_decrypted_text is not None:
+        with open(output_file, 'w') as f:
+            f.write(encrypted_decrypted_text)
+
+if __name__ == "__main__":
+    main()
